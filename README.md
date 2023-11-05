1445942d8beb6dcfa92a159a2c630890574f9d80
KDercksen <mail@koendercksen.com>
2023-11-05 21:40:37 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,41 @@
+Simple backup utility to sync top level folders to an S3 bucket. Stores each
+directory as a tar.gz file in the bucket, and also uploads a checksum file for
+each archive.
+
+If checksum has not changed, the archive will be skipped in the next run.
+
+One way backup only for now, no deletion of files in the bucket and no
+functionality to copy files back from the bucket (maybe I will implement this
+later).
+
+Example:
+
+```
+rootdir/
+    dir1/
+        file1
+        file2
+    dir2/
+        file3
+        file4
+```
+
+will be uploaded as `dir1.tar.gz` + `dir1.md5` and `dir2.tar.gz` + `dir2.md5`.
+
+## Usage
+
+```bash
+python backup.py /path/to/dir --bucket my-bucket --profile my-aws-profile --storage-class DEEP_ARCHIVE
+```
+
+Arguments:
+
+| Argument             | Description                                                                                              |
+| -------------------- | -------------------------------------------------------------------------------------------------------- |
+| `path`               | Path to the directory to backup                                                                          |
+| `--bucket`           | Name of the S3 bucket to upload to                                                                       |
+| `--profile`          | AWS profile to use (optional, will use `default` when not specified)                                     |
+| `--storage-class`    | Storage class to use (optional, defaults to STANDARD)                                                    |
+| `--verbose`          | Print verbose output (optional)                                                                          |
+| `--dryrun`           | Do not upload anything, just print what would be uploaded (optional)                                     |
+| `--checksum-content` | Use filename plus file content to calculate checksum. If not specified, only filename is used (optional) |
---
+++ backup.py
@@ -0,0 +1,92 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+import argparse
+import hashlib
+import io
+import tarfile
+from pathlib import Path
+
+import boto3
+from tqdm import tqdm
+
+
+def get_children(path):
+    return sorted(p for p in path.glob("**/*") if p.is_file())
+
+
+def get_checksum(path, use_content=False):
+    children = get_children(path)
+    checksum = hashlib.md5()
+    for child in children:
+        checksum.update(child.as_posix().encode())
+        if use_content:
+            with open(child, "rb") as f:
+                checksum.update(f.read())
+    return checksum.hexdigest()
+
+
+def create_tarball(files):
+    tar_data = io.BytesIO()
+    for child in files:
+        with tarfile.open(fileobj=tar_data, mode="w:gz") as tar:
+            tar.add(child)
+    tar_data.seek(0)
+    return tar_data
+
+
+def download_hash(bucket, key):
+    outfile = io.BytesIO()
+    try:
+        bucket.download_fileobj(key, outfile)
+        outfile.seek(0)
+        return outfile.read().decode()
+    except Exception:
+        return None
+
+
+def get_hashfile_name(item):
+    return item.name + ".md5"
+
+
+def get_archive_name(item):
+    return item.name + ".tar.gz"
+
+
+if __name__ == "__main__":
+    p = argparse.ArgumentParser()
+    p.add_argument("path", type=Path, help="path to backup")
+    p.add_argument("--dryrun", action="store_true", help="dry run")
+    p.add_argument("--checksum-content", action="store_true", help="checksum content")
+    p.add_argument("--bucket", help="bucket name", required=True)
+    p.add_argument("--profile", help="aws profile name")
+    p.add_argument("--storage-class", help="aws storage class", default="STANDARD")
+    p.add_argument("--verbose", action="store_true", help="verbose")
+    args = p.parse_args()
+
+    session = boto3.Session(profile_name=args.profile)
+    s3 = session.resource("s3")
+    bucket = s3.Bucket(args.bucket)
+
+    items = sorted(args.path.glob("*"))
+    for item in tqdm(items, desc="Uploading", unit="files", total=len(items)):
+        checksum = get_checksum(item, use_content=args.checksum_content)
+        if args.verbose:
+            tqdm.write(f"{item.name} - {checksum}")
+
+        remote_checksum = download_hash(bucket, get_hashfile_name(item))
+
+        if checksum != remote_checksum:
+            if args.verbose:
+                tqdm.write(f"{item.name} changed, uploading...")
+            if not args.dryrun:
+                tarball = create_tarball(get_children(item))
+                bucket.upload_fileobj(
+                    tarball,
+                    get_archive_name(item),
+                    ExtraArgs={"StorageClass": args.storage_class},
+                )
+                bucket.put_object(Body=checksum, Key=get_hashfile_name(item))
+        else:
+            if args.verbose:
+                tqdm.write(f"{item.name} unchanged, skipping...")
